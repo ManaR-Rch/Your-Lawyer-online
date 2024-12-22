04cf7c60c7706840a4586c942030b78f25f0c3ba
ManaR-Rch <manarmarchou6@gmail.com>
2024-12-22 23:20:12 +0100

    reserve function added

--- pages/lawyers/reservation.php
+++ pages/lawyers/reservation.php
@@ -9,7 +9,7 @@
 <body>
     <header>
         <nav>
-            <div class="logo">CabinetJ uridique Excellence</div>
+            <div class="logo">Cabinet J uridique Excellence</div>
             <ul>
                 <li><a href="../index.html">Accueil</a></li>
                 <li><a href="../index.html">deconnexion</a></li>
